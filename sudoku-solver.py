d394386e3d560eb298c1716657873a77d7839f0a
Nouman <noumancodes@gmail.com>
2021-08-22 23:27:10 -0400

    Made a printing function to print the sudoko grid

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,1 +1,11 @@
-print("Hello World")
+def prettyPrint(board):
+    print("+" + "---+"*9)
+    for i, row in enumerate(board):
+        print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
+        if i % 3 == 2:
+            print("+" + "---+"*9)
+        else:
+            print("+" + "   +"*9)
+
+board = [[0] * 9] * 9
+prettyPrint(board)
